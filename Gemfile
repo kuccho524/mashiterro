8885533daf28a618169fcd83b2306bef8367bf3d
kuccho524 <kucchachachanocha@gmail.com>
2021-04-15 09:36:50 +0000

    Third Commit

--- Gemfile
+++ Gemfile
@@ -63,5 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem 'kaminari','~> 1.2.1'
 
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :post_images, only: [:index, :show, :new, :create, :destroy]
+  resources :post_images, only: [:index, :show, :new, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+
 end
